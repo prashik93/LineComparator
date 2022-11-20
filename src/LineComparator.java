60577b8f668f85411f9051dd80381dc8aa74764d
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-20 20:02:04 +0530

    ADDED : Code to check two line are equal or not using java equal method.

--- src/LineComparator.java
+++ src/LineComparator.java
@@ -3,17 +3,30 @@ public class LineComparator {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program!");
         Scanner scnr = new Scanner(System.in);
-        System.out.println("Enter value for starting position of a line : ");
+        System.out.println("Enter value for First Line starting position : ");
         int x1 = scnr.nextInt();
         int y1 = scnr.nextInt();
-        System.out.println("Enter value for ending position of a line : ");
+        System.out.println("Enter value for First Line ending position : ");
         int x2 = scnr.nextInt();
         int y2 = scnr.nextInt();
-        if((x1 < x2) && (y1 < y2)) {
-            int lengthOfLine = (int) Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2));
-            System.out.println("Length of a line is : " + lengthOfLine);
+        int lengthOfFirstLine = (int) Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2));
+        String len1 = String.valueOf(lengthOfFirstLine);
+        System.out.println("Length of first line : " + lengthOfFirstLine);
+
+        System.out.println("Enter value for Second Line starting position : ");
+        int x3 = scnr.nextInt();
+        int y3 = scnr.nextInt();
+        System.out.println("Enter value for Second Line ending position : ");
+        int x4 = scnr.nextInt();
+        int y4 = scnr.nextInt();
+        int lengthOfSecondLine = (int) Math.sqrt(Math.pow(x4 - x3,2) + Math.pow(y4 - y3,2));
+        String len2 = String.valueOf(lengthOfSecondLine);
+        System.out.println("Length of second line : " + lengthOfSecondLine);
+
+        if(len1.equals(len2)) {
+            System.out.println("Lengths are equals");
         } else {
-            System.out.println("Starting value must be less than Ending value!");
+            System.out.println("Lengths are not equals");
         }
     }
 }
